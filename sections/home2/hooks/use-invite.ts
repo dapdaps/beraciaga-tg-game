3c98e40e61762404eee2d60045adc5255d372482
Yyy <snwyz1st@gmail.com>
2025-02-11 21:50:44 +0800

    fix: fix start params

--- sections/home2/hooks/use-invite.ts
+++ sections/home2/hooks/use-invite.ts
@@ -18,7 +18,7 @@ export function useInvite(defaultInviterSource = 'okx_invite') {
       return {};
     }
 
-    const startappValue = `inviterId<${userInfo?.id || ''}<inviterSource<${defaultInviterSource}`;
+    const startappValue = `inviterId$${userInfo?.id || ''}$inviterSource$${defaultInviterSource}`;
     const app_link = `${process.env.NEXT_PUBLIC_APP_LINK}?startapp=${encodeURIComponent(startappValue)}`;
     const tg_share_link = `https://t.me/share/url?url=${app_link}&text=DapDap %26 Beratown team is dropping sumting new 👀 %0A Idk what it is but just sign up to the TG mini app to stack up the BGOLD first`;
     console.log(app_link, '---shareLink---');
