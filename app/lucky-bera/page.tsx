4053b22280cbe442aac282d804800694feea6d9a
jimmygu <jimmyguu2024@gmail.com>
2025-02-27 14:47:33 +0800

    feat: Add Lucky Bera page and background assets
    Introduce a new Lucky Bera section with a dedicated React component and page. The implementation includes a background image SVG for styling purposes. This sets up the foundation for the Lucky Bera feature.

---
+++ app/lucky-bera/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LuckyBeraView from '@/sections/lucky-bera';
+
+const LuckyBera: React.FC = () => {
+  return (
+    <LuckyBeraView />
+  );
+};
+
+export default LuckyBera;
---
+++ sections/lucky-bera/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LuckyBeraView: React.FC<any> = () => {
+  return (
+    <div className="w-full h-full bg-[url('/images/lucky-bera/bg.svg')] bg-no-repeat bg-cover bg-top">
+
+    </div>
+  );
+};
+
+export default LuckyBeraView;
