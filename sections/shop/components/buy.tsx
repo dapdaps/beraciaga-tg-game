83ec49bb7a46c53fe40e11541776d1cea6c6517f
jimmygu <jimmyguu2024@gmail.com>
2025-03-20 18:01:45 +0800

    feat: Add coupon selection functionality to shop component
    Introduced a new coupon selection feature in the shop modal, including UI components and logic for applying discounts. Replaced `Drawer` with a redesigned `Modal` for better UX and created a reusable `Coupon` component. Added new assets to support the visual design of the coupon system.

--- sections/shop/components/buy.tsx
+++ sections/shop/components/buy.tsx
@@ -2,7 +2,7 @@ import LightingButton from '@components/Button/lighting-button';
 import FlagModal from '@components/flag-modal';
 import { numberFormatter } from '@/utils/number-formatter';
 import Drawer from '@components/Drawer';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useRequest } from 'ahooks';
 import { get } from '@/utils/http';
 import { useTelegram } from '@/hooks/useTelegram';
@@ -11,6 +11,9 @@ import Skeleton from 'react-loading-skeleton';
 import { CouponItem } from '@/sections/shop/config';
 import Loading from '@components/Loading';
 import Big from 'big.js';
+import Modal from '@components/modal';
+import Coupon from '@/sections/shop/components/coupon';
+import Empty from '@components/Empty';
 
 const Buy = (props: any) => {
   const {
@@ -25,7 +28,6 @@ const Buy = (props: any) => {
   const userData: UserData = WebApp?.initDataUnsafe?.user;
 
   const [coupon, setCoupon] = useState<CouponItem>();
-  const [couponsVisible, setCouponsVisible] = useState(false);
 
   const { data: couponList, loading: couponLoading, run: getCouponList } = useRequest<CouponItem[], any>(async () => {
     const res = await get("/api/coupon/user", {
@@ -51,127 +53,96 @@ const Buy = (props: any) => {
       return 0;
     }
     return _price;
-  }, [buyProduct, coupon])
+  }, [buyProduct, coupon]);
+
+  useEffect(() => {
+    if (!buyProduct || !visible) {
+      return;
+    }
+    getCouponList();
+  }, [buyProduct, visible]);
 
   return (
-    <FlagModal
-      visible={visible}
+    <Modal
+      open={visible}
       onClose={onClose}
-      footer={(
-        <LightingButton
-          className=""
-          onClick={() => {}}
-        >
-          More
-        </LightingButton>
+      closeIcon={(
+        <img src="/images/raffle/close.png" className="w-[34px] h-[34px]" />
       )}
+      closeIconClassName="top-[-17px] right-[-17px]"
     >
-      <div className="flex flex-col items-center h-full w-full">
-        <img src="/images/shop/out-of-honey.png" alt="" className="w-[233px] h-[39px] object-contain object-center shrink-0" />
-        <div className="flex flex-col items-center flex-1 w-full relative">
-          <img src={buyProduct?.logo} alt="" className="w-[200px] object-contain object-center shrink-0" />
-          <div
-            className="absolute bottom-[100px] rotate-[-6deg] text-center text-stroke-2 stroke-2 font-cherryBomb text-[40px] font-normal leading-[24px] uppercase"
-            style={{
-              color: buyProduct?.color || '#FF7EC1',
-              textShadow: `0px 2px 0px ${buyProduct?.shadowColor || '#B42647'}`,
-            }}
-          >
-            {buyProduct?.name}
+      <div className="w-[340px] rounded-[10px] border-[2px] border-[#7F6C41] bg-[linear-gradient(180deg,_#D4A20C_0%,_#FFCC34_100%)] p-[3px]">
+        <div className="w-full rounded-[8px] border-[2px] border-[#E5C375] bg-[#FFF1C7] p-[22px_25px_30px] flex flex-col items-center">
+          <div className="relative shrink-0 w-[100px] h-[106px] rounded-[16px] border-[2px] border-[#D7C69D] bg-[#FFFAEA] flex items-center flex-col gap-[10px]">
+            <div
+              className="rotate-[-6deg] text-center text-stroke-2 stroke-2 font-cherryBomb text-[24px] font-normal leading-[1] uppercase"
+              style={{
+                color: buyProduct?.color || '#FF7EC1',
+                textShadow: `0px 2px 0px ${buyProduct?.shadowColor || '#B42647'}`,
+              }}
+            >
+              {buyProduct?.name}
+            </div>
+            <img src={buyProduct?.logo} alt="" className="w-[70px] object-contain object-center shrink-0" />
           </div>
-          <div
-            className="flex items-center gap-[5px] text-[#F7F9EA] text-stroke-2 text-[16px] font-[400] font-cherryBomb mt-[10px]"
-            onClick={() => {
-              setCouponsVisible(true);
-              getCouponList();
-            }}
-          >
-            <div className="shrink-0">Coupon: </div>
-            <div className="flex items-center gap-[5px] flex-1">
-              <div className="flex-1">
-                {coupon ? numberFormatter(coupon.discount_value, 2, true, { prefix: '-$' }) : "Unselect"}
-              </div>
-              <img src="/images/icon-arrow-down.svg" alt="" className="shrink-0 w-[12px] h-[7px] translate-y-0.5" />
+          <div className="mt-[6px] flex flex-col items-center gap-[2px] leading-[1] text-[16px] text-[#F7F9EA] text-stroke-2 font-cherryBomb">
+            <div className="text-[10px] text-stroke-1 line-through decoration-red-500">
+              {numberFormatter(buyProduct?.original_price, 2, true, { prefix: '$' })}
+            </div>
+            <div className="">
+              {numberFormatter(buyProduct?.discount_price, 2, true, { prefix: '$' })}
             </div>
           </div>
-        </div>
-        <LightingButton
-          disabled={buying}
-          outerClassName="!w-full shrink-0"
-          onClick={() => {
-            handleProductPay(buyProduct, coupon);
-          }}
-        >
-          {
-            buying && (
-              <Loading size={14} />
-            )
-          }
-          {numberFormatter(payPrice, 2, true, { prefix: '$' })}
-        </LightingButton>
-      </div>
-      <Drawer
-        visible={couponsVisible}
-        onClose={() => {
-          setCouponsVisible(false);
-        }}
-        size="50dvh"
-        overlayClassName="z-[101]"
-        className="p-[4px] border-[2px] border-[#7F6C41] !rounded-t-[10px] bg-[linear-gradient(180deg,_#D4A20C_0%,_#FFCC34_100%)]"
-      >
-        <div className="w-full h-full flex flex-col gap-[10px] border-[2px] border-[#E5C375] bg-[#FFF1C7] p-[15px_20px] rounded-[8px]">
-          <div
-            className="text-center text-[26px] font-cherryBomb text-stroke-2 text-[#FFDF77]"
-            style={{
-              textShadow: "0px 4px 0px #4B371F"
+          <div className="flex flex-col mt-[20px] w-full">
+            <div className="shrink-0 font-cherryBomb text-left text-[20px] bg-[linear-gradient(180deg,_#FFDF77_0%,_#F6AD0F_100%)] bg-clip-text [text-shadow:0px_2px_0px_#4B371F] [-webkit-text-fill-color:transparent]">
+              Coupon
+            </div>
+            <div className="flex-1 max-h-[180px] overflow-y-auto mt-[15px] flex flex-col items-stretch gap-[15px]">
+              {
+                couponLoading ? (
+                  <>
+                    <Skeleton width="271px" height="70px" borderRadius="10px" />
+                    <Skeleton width="271px" height="70px" borderRadius="10px" />
+                  </>
+                ) : (
+                  !!couponList?.length ? couponList?.map((item, index) => (
+                    <Coupon
+                      item={item}
+                      key={index}
+                      selected={item.id === coupon?.id}
+                      onSelect={() => {
+                        if (item.id === coupon?.id) {
+                          setCoupon(void 0);
+                          return;
+                        }
+                        setCoupon(item);
+                      }}
+                    />
+                  )) : (
+                    <Empty desc="No coupon available" />
+                  )
+                )
+              }
+            </div>
+          </div>
+          <LightingButton
+            disabled={buying}
+            outerClassName="!w-full shrink-0 mt-[30px] !h-[57px] shaodow-[-1px_4px_0px_0px_#4B371F]"
+            className="!text-[18px]"
+            onClick={() => {
+              handleProductPay(buyProduct, coupon);
             }}
           >
-            Coupons
-          </div>
-          <div className="mt-[15px] text-[#4B371F] font-cherryBomb text-[20px] h-[calc(100%_-_55px)] overflow-y-auto flex flex-col gap-[10px]">
             {
-              couponLoading ? (
-                <>
-                  <Skeleton width="100%" height="54px" borderRadius="10px" />
-                  <Skeleton width="100%" height="54px" borderRadius="10px" />
-                </>
-              ) : (
-                <>
-                  <div
-                    className="flex justify-between items-center border-[2px] border-[#D7C69D] rounded-[16px] py-[10px] px-[15px]"
-                    onClick={() => {
-                      setCoupon(void 0);
-                      setCouponsVisible(false);
-                    }}
-                  >
-                    Do not use
-                  </div>
-                  {
-                    couponList?.map?.((item) => (
-                      <div
-                        className="flex justify-between items-center border-[2px] border-[#D7C69D] rounded-[16px] py-[10px] px-[15px]"
-                        key={item.id}
-                        onClick={() => {
-                          setCoupon(item);
-                          setCouponsVisible(false);
-                        }}
-                      >
-                        <div className="">
-                          {numberFormatter(item.discount_value, 2, true, { prefix: '-$' })}
-                        </div>
-                        <div className="">
-                          Valid
-                        </div>
-                      </div>
-                    ))
-                  }
-                </>
+              buying && (
+                <Loading size={14} />
               )
             }
-          </div>
+            {numberFormatter(payPrice, 2, true, { prefix: '$' })}
+          </LightingButton>
         </div>
-      </Drawer>
-    </FlagModal>
+      </div>
+    </Modal>
   );
 };
 
