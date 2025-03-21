e586fd79f82e4e502f42749becaa2f0d2f633d66
Yyy <snwyz1st@gmail.com>
2025-03-21 17:36:23 +0800

    fix build

--- components/header/connect.tsx
+++ components/header/connect.tsx
@@ -24,13 +24,15 @@ const Connect = ({
         store.set({ isMuted: !isSoundOn })
     }
     
+    if (!userData) return null
+
     return (
         <Modal open={open} onClose={onClose} isShowCloseIcon={false}>
             <div className="w-[220px] h-[227px] bg-[#FFFAEA] border-2 border-[#D7C69D] rounded-2xl">
                 <div className="w-full h-full relative p-2">
                     <div className="flex items-center gap-2">
                         <img src={userData?.photo_url || '/svg/okx.svg'} className="w-[36px] h-[36px] rounded-full" alt="" />
-                        <div className="font-cherryBomb text-[26px] leading-[26px] text-white text-stroke-2">@{userData.username}</div>
+                        <div className="font-cherryBomb text-[26px] leading-[26px] text-white text-stroke-2">@{userData?.username || '-'}</div>
                     </div>
                     <svg className="mt-3" width="204" height="2" viewBox="0 0 204 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <line x1="1" y1="1" x2="203" y2="1" stroke="#7F6D41" stroke-width="2" stroke-linecap="round" stroke-dasharray="2 4"/>
